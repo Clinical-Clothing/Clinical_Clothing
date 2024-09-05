97c07cb1e01017925c36c06df7780cf0df08f931
Clinical-Clothing <ryaneldemire56@gmail.com>
2024-09-05 23:33:10 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve pages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/products.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'products.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
